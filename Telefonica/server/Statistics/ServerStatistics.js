583227c41b7c1c78cad0965efc1955eb82e93aac
vishaalchauhan <chauhan.vishaal@gmail.com>
2016-08-29 01:07:27 +0100

    got the callback working

--- Telefonica/server/Statistics/ServerStatistics.js
+++ Telefonica/server/Statistics/ServerStatistics.js
@@ -2,6 +2,7 @@
 
 let config = require("../config.json");
 let snmp = require("net-snmp");
+let Q = require("q");
 let options = {
     port: config.snmpEndpoint.port, //default
     // retries: 1,
@@ -21,7 +22,6 @@ let session  = snmp.createSession (
 	config.snmpEndpoint.host,
 	config.snmpEndpoint.publicString,
 	options);
-
 
 //Mock method
 exports.getServerStatsMock = function(){
@@ -47,8 +47,11 @@ function responseBuilder(){
 		//getResource(Array.from(config.oids[oid]));
 		let oidKey = config.oids[oidElement] ;
 		oidArray.push(oidKey);
+
 		let oidValue = getResource(oidArray);
+
 		//Add each oid key/ value to object
+		console.log("oidValue =" + oidValue);
 		stats[oidElement] = oidValue ;
 		
 		oidArray.pop();
@@ -59,9 +62,9 @@ function responseBuilder(){
 function getResource(oid){
 	let response = "" ;
 
-	var oidCallBack = function (oidValue){ 
+	let oidCallBack = function (oidValue){ 
 		
-		console.log("oidvalue - ",oidValue);
+		console.log("oidvalue - " + oidValue);
 		response = oidValue ; 
 	};
 
@@ -82,34 +85,66 @@ function getResource(oid){
 				}
 				else
 				{
-					console.log(varbinds[i].oid + " = " + varbinds[i].value)
+					
 					//oidValue = varbinds[i].value ;
 					//return oidCallBack(varbinds[i].value);
-					retValue = varbinds[i].value ;
-					next();
+					//retValue = varbinds[i].value ;
+					//next();
+					response = varbinds[i].value ;
+					//console.log(varbinds[i].oid + " = " + response);
+					oidCallBack(response);
 				}
 			}
 		}
 
-		function next(){
-			if(retValue){
-				oidCallBack(retValue);
-			}
-		};
+		console.log(varbinds[0].oid + " = " + response);
+		return response ;
 	};
 	
-	session.get (oid, snmpSessionCallBack);
-// function foo(address, fn){
-//   geocoder.geocode( { 'address': address}, function(results, status) {
-//      fn(results[0].geometry.location); 
-//   });
-// }
+	function snmpSessionPromise (error, varbinds){
+		let retValue ;
+		if(error){
+			console.error(error);
+		} else {
+			for(let i = 0; i < varbinds.length;i++)
+			{
+				if(snmp.isVarbindError(varbinds[i])){
+					console.error("VarbindError:", snmp.varbindError(varbinds[i]))
+					//Todo : log error
+					//oidValue = "";
+				
+				}
+				else
+				{
+					console.log(varbinds[i].oid, " = " , varbinds[i].value)
+					//oidValue = varbinds[i].value ;
+					//return oidCallBack(varbinds[i].value);
+					retValue = varbinds[i].value ;
+				}
+			}
+		}
+		return retValue ;
+	};
 
-// foo("address", function(location){
-//   alert(location); // this is where you get the return value
-// });
-	//oidValue = oidCallBack() ;
+	session.get (oid, snmpSessionCallBack);
+	//return response;
+
+	// var deferred = Q.defer();
+	// session.get(oid,snmpSessionPromise, deferred.makeNodeResolver());
+	// return deferred.promise;
+
+	//return Q.nfcall(session.get(oid,snmpSessionPromise));
+
+	//return Q.nfapply(session.get(oid,snmpSessionPromise));
+	// return Q.nfcall(session.get(oid,snmpSessionPromise)).then(
+	// 	function(){
+	// 		return retValue ;
+	// 	});
+
+
+
+
 	
-	return response;
+	
 };
 
