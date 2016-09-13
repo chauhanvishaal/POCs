6c8b696b0c1317c51ba134e028dfb56520375baf
vishaalchauhan <chauhan.vishaal@gmail.com>
2016-09-13 10:49:01 +0100

    TypeScript basic tests with, Gulp workflow for TS to JS, LESS to CSS and CSS minification

---
+++ TS1/public/js/Succintly.js
@@ -0,0 +1,14 @@
+var Succintly;
+(function (Succintly) {
+    var Logger = (function () {
+        function Logger() {
+        }
+        Logger.prototype.log = function (message) {
+            if (typeof window.console !== 'undefined') {
+                window.console.log('TypeScript generated msg: ' + message);
+            }
+        };
+        return Logger;
+    })();
+    Succintly.Logger = Logger;
+})(Succintly || (Succintly = {}));
